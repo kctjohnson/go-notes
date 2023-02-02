fbef42db74e5228d8ee0a7948ef8af9726dd858f
Killean Johnson <killeanjohnson@Killeans-MacBook-Pro.local>
2023-02-02 08:50:01 -0500

    First server commit

---
+++ cmd/gonotes-server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("This app will eventually handle creating and handling all\n")
+	fmt.Printf("note related server requests with a sqlite database rather than MySQL.\n")
+	fmt.Printf("This will allow the user to just run the server wherever they'd like,\n")
+	fmt.Printf("and then they can use the note app like normal, whether the notes are\n")
+	fmt.Printf("stored locally or on a server.\n")
+}

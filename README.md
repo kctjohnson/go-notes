069a798f2ae0f9cf7c88d1b406bf524d3c580466
Killean Johnson <killeanjohnson@Killeans-MacBook-Pro.local>
2023-02-02 19:16:57 -0500

    Setting up the sqlite3 connection

---
+++ README.md
@@ -0,0 +1,4 @@
+# GoNotes Server
+## Installation
+`go install -tags 'sqlite3' github.com/golang-migrate/migrate/v4/cmd/migrate@latest`
+`make migrate`
--- cmd/gonotes-server/main.go
+++ cmd/gonotes-server/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
 	fmt.Printf("This app will eventually handle creating and handling all\n")
@@ -8,4 +14,29 @@ func main() {
 	fmt.Printf("This will allow the user to just run the server wherever they'd like,\n")
 	fmt.Printf("and then they can use the note app like normal, whether the notes are\n")
 	fmt.Printf("stored locally or on a server.\n")
+
+	db, err := openDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func openDB() (*sqlx.DB, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	dbPath := "/.config/gonotes/"
+	dbName := "gonotes.db"
+	dbFullPath := homeDir + dbPath + dbName
+	db, err := sqlx.Open("sqlite3", dbFullPath)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
